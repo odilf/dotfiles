9997f264736e3364fdc901be6d007aeb8195f9ac
Odilf <odysseas.maheras@gmail.com>
2023-12-01 15:21:07 +0100

    removed unecessary mapping

--- nvim/lua/plugins/editor/lsp.lua
+++ nvim/lua/plugins/editor/lsp.lua
@@ -54,28 +54,8 @@ return {
 						behavior = cmp.ConfirmBehavior.Replace,
 						select = true,
 					},
-
-					-- Next and previous
-					['<C-n>'] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_next_item()
-						elseif luasnip.expand_or_jumpable() then
-							luasnip.expand_or_jump()
-						else
-							fallback()
-						end
-					end, { 'i', 's' }),
-					['<C-p'] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_prev_item()
-						elseif luasnip.jumpable(-1) then
-							luasnip.jump(-1)
-						else
-							fallback()
-						end
-					end, { 'i', 's' }),
 				}),
-				
+
 				formatting = {
 					format = require("lspkind").cmp_format({
 						mode = 'symbol', -- show only symbol annotations

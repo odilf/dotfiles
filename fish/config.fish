7991d6b461235f77ae5f85224fb1f9fdc187d4d1
Odilf <odysseas.maheras@gmail.com>
2024-02-07 00:55:07 +0100

    Added fish and rocketfetch

---
+++ fish/config.fish
@@ -0,0 +1,21 @@
+set PATH $PATH /opt/homebrew/bin
+set PATH $PATH ~/.cargo/bin
+
+abbr --add ls lsd
+abbr --add vim nvim
+abbr --add grep rg
+abbr --add cat bat
+
+abbr --add c cargo
+abbr --add g git
+abbr --add e nvim
+
+
+if status is-interactive
+	zoxide init fish | source
+	starship init fish | source
+	
+	set fish_greeting
+	rocketfetch
+end
+

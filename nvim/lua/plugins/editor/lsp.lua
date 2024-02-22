508c3ba022e56f47a25a1b4c5f62d9fe14809475
Odilf <odysseas.maheras@gmail.com>
2024-02-22 23:03:29 +0100

    whoooole buncha lsp stuff

--- nvim/lua/plugins/editor/lsp.lua
+++ nvim/lua/plugins/editor/lsp.lua
@@ -1,79 +1,169 @@
 return {
-	{
-		"neovim/nvim-lspconfig",
-		event = { "BufRead", "BufNewFile" },
-		dependencies = {
-			"nvim-lua/lsp-status.nvim",          -- status bar
+    {
+        'neovim/nvim-lspconfig',
+        event = { 'BufRead', 'BufNewFile' },
+        dependencies = {
+            'nvim-lua/lsp-status.nvim', -- status bar
+            'folke/neodev.nvim',        -- easier editing of neovim files
 
-			"hrsh7th/nvim-cmp",
-			"hrsh7th/cmp-nvim-lsp",
-			"saadparwaiz1/cmp_luasnip",
-			"L3MON4D3/LuaSnip",
-		},
+            -- Language Server installation
+            'williamboman/mason-lspconfig.nvim',
+            'williamboman/mason.nvim',
+        },
+        cmd = { 'Mason' },
 
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+        config = function()
+            -- Neodev needs to be loaded before lspconfig
+            require('neodev').setup()
 
-			local lspconfig = require('lspconfig')
+            -- Each entry can be name for defaults or an { name, opts } table
+            local servers = {
+                -- web dev
+                'tsserver',
+                'svelte',
+                'tailwindcss',
+                'unocss',
+                'biome',
+                'cssls',
+                'html',
 
-			local servers = {
-				'svelte',
-				'rust_analyzer',
-				'tsserver',
-				'emmet_ls',
-				'lua_ls',
-				'marksman',
-			}
+                -- markdown
+                'marksman',
+                'vale_ls',
+                'typos_lsp',
 
-			for _, lsp in ipairs(servers) do
-				lspconfig[lsp].setup {
-					-- on_attach = my_custom_on_attach,
-					capabilities = capabilities,
-				}
-			end
+                -- standalones 
+                'rust_analyzer',
+                'lua_ls',
+                'pyright',
+                'wgsl_analyzer',
+                'clangd',
+                'taplo',
+                'typst_lsp',
+                'wgsl_analyzer',
+                'yamlls',
+            }
 
-			-- luasnip setup
-			local luasnip = require('luasnip')
+            local server_names = {}
+            for key, value in pairs(servers) do
+                if type(value) == 'table' then
+                    server_names[key] = value[1]
+                else
+                    server_names[key] = value
+                end
+            end
 
-			-- nvim-cmp setup
-			local cmp = require('cmp')
-			cmp.setup {
-				snippet = {
-					expand = function(args)
-						luasnip.lsp_expand(args.body)
-					end,
-				},
+            require('mason').setup()
+            require('mason-lspconfig').setup({
+                ensure_installed = server_names
+            })
 
-				mapping = cmp.mapping.preset.insert({
-					['<C-u>'] = cmp.mapping.scroll_docs(-4), -- Up
-					['<C-d>'] = cmp.mapping.scroll_docs(4), -- Down
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+            local lspconfig = require('lspconfig')
 
-					['<C-Space>'] = cmp.mapping.complete(),
-					['<CR>'] = cmp.mapping.confirm {
-						behavior = cmp.ConfirmBehavior.Replace,
-						select = true,
-					},
-				}),
+            for _, lsp in ipairs(servers) do
+                if type(lsp) == 'string' then
+                    -- Default config
+                    lspconfig[lsp].setup({
+                        capabilities = capabilities,
+                        diagnostics = { enable = true }
+                    })
+                else
+                    -- Override config
+                    lspconfig[lsp[1]].setup(lsp[2])
+                end
+            end
+        end,
 
-				sources = {
-					{ name = 'nvim_lsp' },
-					{ name = 'luasnip' },
-				},
-			}
-		end,
-	},
+    },
 
-	-- top status bar
-	{
-		"glepnir/lspsaga.nvim",
-		event = "LspAttach",
-		config = function()
-			require("lspsaga").setup({})
-		end,
-		dependencies = {
-			{ "nvim-tree/nvim-web-devicons" },
-			--Please make sure you install markdown and markdown_inline parser
-			{ "nvim-treesitter/nvim-treesitter" }
-		}
-	}
+    {
+        'hrsh7th/nvim-cmp',
+        event = { 'BufRead', 'BufNewFile' },
+        dependencies = {
+            'saadparwaiz1/cmp_luasnip',
+            'L3MON4D3/LuaSnip',
+            'onsails/lspkind.nvim', -- lsp symbols
+
+            -- Sources
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-path',
+            'hrsh7th/cmp-cmdline',
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-nvim-lsp-signature-help',
+            'hrsh7th/cmp-calc',
+        },
+        config = function()
+            local lspkind = require('lspkind')
+            local luasnip = require('luasnip')
+            local cmp = require('cmp')
+
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        luasnip.lsp_expand(args.body)
+                    end,
+                },
+
+                mapping = cmp.mapping.preset.insert({
+                    ['<C-u>'] = cmp.mapping.scroll_docs(-4), -- Up
+                    ['<C-d>'] = cmp.mapping.scroll_docs(4),  -- Down
+
+                    ['<C-Space>'] = cmp.mapping.complete(),
+                    ['<CR>'] = cmp.mapping.confirm {
+                        behavior = cmp.ConfirmBehavior.Replace,
+                        select = true,
+                    },
+                }),
+
+                sources = {
+                    { name = 'nvim_lsp' },
+                    { name = 'luasnip' },
+                    { name = 'path' },
+                    { name = 'nvim_lsp_signature_help' },
+                    { name = 'calc' },
+                },
+
+                view = {
+                    entries = "custom",
+                },
+
+                -- honestly, 🤷
+                ---@diagnostic disable-next-line: missing-fields
+                formatting = {
+                    format = lspkind.cmp_format({
+                        mode = 'symbol', -- show only symbol annotations
+                    })
+                }
+            })
+
+            -- Completions for search
+            cmp.setup.cmdline({ '/', '?' }, {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = {
+                    { name = 'buffer' }
+                }
+            })
+
+            -- Completions for command line
+            cmp.setup.cmdline(':', {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = cmp.config.sources({
+                    { name = 'path' }
+                }, {
+                    { name = 'cmdline' }
+                })
+            })
+        end,
+    },
+
+    -- top status bar
+    {
+        'glepnir/lspsaga.nvim',
+        event = 'LspAttach',
+        dependencies = {
+            { 'nvim-tree/nvim-web-devicons' },
+            { 'nvim-treesitter/nvim-treesitter' }
+        }
+    },
 }

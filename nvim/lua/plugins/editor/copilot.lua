d87550350fd8df838573bfdb9e3aefc7e0ee9e36
Odilf <odysseas.maheras@gmail.com>
2024-02-22 23:02:10 +0100

    Simplified copilot config

--- nvim/lua/plugins/editor/copilot.lua
+++ nvim/lua/plugins/editor/copilot.lua
@@ -2,35 +2,7 @@ return {
 	{
 		"zbirenbaum/copilot.lua",
 		event = "InsertEnter",
-		opts = {
-			suggestion = {
-				enabled = true,
-				auto_trigger = true,
-				debounce = 75,
-				keymap = {
-					accept = "<M-l>",
-					accept_word = false,
-					accept_line = false,
-					next = "<M-]>",
-					prev = "<M-[>",
-					dismiss = "<C-]>",
-				},
-			},
-
-			filetypes = {
-				yaml = false,
-				markdown = false,
-				help = false,
-				gitcommit = false,
-				gitrebase = false,
-				hgcommit = false,
-				svn = false,
-				cvs = false,
-				["."] = false,
-			},
-
-			copilot_node_command = 'node', -- Node.js version must be > 16.x
-			server_opts_overrides = {},
-		},
+		cmd = "Copilot",
+		opts = {},
 	}
 }

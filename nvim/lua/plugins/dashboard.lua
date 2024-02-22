9e327920dbf55965f3b66c9b2df0e2096dede9dd
Odilf <odysseas.maheras@gmail.com>
2024-02-22 23:04:24 +0100

    nicer dashboard

--- nvim/lua/plugins/dashboard.lua
+++ nvim/lua/plugins/dashboard.lua
@@ -1,10 +1,36 @@
+-- TODO: Show dashboard even if opening directory
+
 return {
 	{
 		'goolord/alpha-nvim',
-		event = "VimEnter",
+		lazy = false,
 		dependencies = { 'nvim-tree/nvim-web-devicons' },
 		config = function()
-			require('alpha').setup(require('alpha.themes.dashboard').config)
+			local alpha = require('alpha')
+			local dashboard = require('alpha.themes.dashboard')
+
+			dashboard.section.header.val = {
+				[[                                                                       ]],
+				[[                                                                       ]],
+				[[                                                                       ]],
+				[[                                                                       ]],
+				[[                                                                       ]],
+				[[                                                                       ]],
+				[[                                                                       ]],
+				[[                                                                     ]],
+				[[       ████ ██████           █████      ██                     ]],
+				[[      ███████████             █████                             ]],
+				[[      █████████ ███████████████████ ███   ███████████   ]],
+				[[     █████████  ███    █████████████ █████ ██████████████   ]],
+				[[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+				[[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+				[[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+				[[                                                                       ]],
+				[[                                                                       ]],
+				[[                                                                       ]],
+			}
+
+			alpha.setup(dashboard.opts)
 		end
 	}
 }

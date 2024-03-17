6bb1c6a2cec34ebeaa44ed52c4b1fa632aa8e97a
Odilf <odysseas.maheras@gmail.com>
2024-03-17 20:43:46 +0100

    restore sessions with nvim-tree properly

--- nvim/lua/plugins/editor/sessions.lua
+++ nvim/lua/plugins/editor/sessions.lua
@@ -1,13 +1,19 @@
-vim.o.sessionoptions="blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+
+local function change_nvim_tree_dir()
+	local nvim_tree = require("nvim-tree")
+	nvim_tree.change_dir(vim.fn.getcwd())
+end
 
 return {
 	{
 		'rmagatti/auto-session',
 		lazy = false,
 		opts = {
-				log_level = "error",
-				auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+			log_level = "error",
+			auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+			post_restore_cmds = { change_nvim_tree_dir, "NvimTreeOpen" },
+			pre_save_cmds = { "NvimTreeClose" },
 		}
 	},
 }
-

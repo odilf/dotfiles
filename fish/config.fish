d8c05e5b630bcb17a934f2ff90ef98e5f9d8456f
Odilf <odysseas.maheras@gmail.com>
2024-03-17 13:43:22 +0100

    fish: added `fuck`

--- fish/config.fish
+++ fish/config.fish
@@ -11,6 +11,8 @@ set -x LANG en_US.UTF-8
 set -x SHELL "fish"
 
 if status is-interactive
+	thefuck --alias | source
+
 	zoxide init fish | source
 	starship init fish | source
 	
@@ -18,3 +20,6 @@ if status is-interactive
 	pfetch
 end
 
+
+# Created by `pipx` on 2024-03-13 10:57:59
+set PATH $PATH /Users/odilf/.local/bin

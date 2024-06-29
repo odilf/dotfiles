ab0883d44044f7d3f99f7e1049a005e7d6fb807a
Odilf <odysseas.maheras@gmail.com>
2024-06-29 10:21:57 +0300

    fix(nvim): nicer markdown

--- nvim/lua/plugins/languages/markdown.lua
+++ nvim/lua/plugins/languages/markdown.lua
@@ -1,6 +1,43 @@
 return {
-	"iamcco/markdown-preview.nvim",
-	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-	ft = { "markdown" },
-	build = function() vim.fn["mkdp#util#install"]() end,
+	{
+
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		build = "cd app && pnpm install",
+		ft = { "markdown" },
+		init = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+		end,
+	},
+
+	{
+		"tadmccorkle/markdown.nvim",
+		ft = "markdown", -- or 'event = "VeryLazy"'
+		opts = {
+			-- configuration here or empty for defaults
+		},
+	},
+
+	{
+		"OXY2DEV/markview.nvim",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons", -- Used by the code bloxks
+		},
+		opts = {},
+	},
+
+	{
+		"zk-org/zk-nvim",
+		opts = {
+			picker = "telescope",
+			auto_attach = {
+				enabled = true,
+				filetypes = { "markdown" },
+			},
+		},
+		ft = { "markdown" },
+		config = function(_, opts)
+			require("zk").setup(opts)
+		end,
+	},
 }

050fac069a0c2052940ece6bf64cceaf3a303815
Odilf <odysseas.maheras@gmail.com>
2024-07-09 11:52:30 +0300

    nvim: auto switch theme based on OS

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -29,4 +29,20 @@ return {
       }
     }
   },
+
+  -- Auto switch light/dark theme
+  {
+    "f-person/auto-dark-mode.nvim",
+    opts = {
+      update_interval = 1000,
+      set_dark_mode = function()
+        vim.api.nvim_set_option_value("background", "dark", {})
+        vim.cmd("colorscheme duskfox")
+      end,
+      set_light_mode = function()
+        vim.api.nvim_set_option_value("background", "light", {})
+        vim.cmd("colorscheme dayfox")
+      end,
+    },
+  }
 }

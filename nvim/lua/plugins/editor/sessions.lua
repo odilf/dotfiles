84378855303b36a2c91cb4814f64935e2dd9f85f
Odilf <odysseas.maheras@gmail.com>
2024-07-13 09:44:32 +0300

    nvim: different sessions on git branches

--- nvim/lua/plugins/editor/sessions.lua
+++ nvim/lua/plugins/editor/sessions.lua
@@ -1,4 +1,4 @@
-vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+vim.o.sessionoptions = "blank,buffers,curdir,folds,globals,help,tabpages,winsize,winpos,terminal,localoptions"
 
 local function change_nvim_tree_dir()
 	local nvim_tree = require("nvim-tree")
@@ -12,6 +12,9 @@ return {
 		opts = {
 			log_level = "error",
 			auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+			auto_session_use_git_branch = true,
+			-- TODO: Only restore NvimTree if NvimTree was opened previously
+			-- TODO: Restore (close and open) terminal buffers too
 			post_restore_cmds = { change_nvim_tree_dir, "NvimTreeOpen" },
 			pre_save_cmds = { "NvimTreeClose" },
 		}

6c1232df6c0e034e31f22952096d056375cb2fe0
Odilf <odysseas.maheras@gmail.com>
2024-07-13 14:04:57 +0300

    nvim: nvim-tree thingy

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,7 @@
+-- Disable netrw, because of `nvim-tree`
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({

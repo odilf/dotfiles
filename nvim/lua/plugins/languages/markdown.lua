b2e45923dd4f593cd2e3a18519b91937c741b524
Odilf <odysseas.maheras@gmail.com>
2024-10-04 21:16:06 +0200

    nvim: peek is nice, only thing needed until further notice

--- nvim/lua/plugins/languages/markdown.lua
+++ nvim/lua/plugins/languages/markdown.lua
@@ -1,43 +1,12 @@
 return {
-	-- {
-	--
-	-- 	"iamcco/markdown-preview.nvim",
-	-- 	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-	-- 	build = "cd app && pnpm install",
-	-- 	ft = { "markdown" },
-	-- 	init = function()
-	-- 		vim.g.mkdp_filetypes = { "markdown" }
-	-- 	end,
-	-- },
-
 	{
-		"tadmccorkle/markdown.nvim",
-		ft = "markdown", -- or 'event = "VeryLazy"'
-		opts = {
-			-- configuration here or empty for defaults
-		},
-	},
-
-	{
-		"OXY2DEV/markview.nvim",
-		dependencies = {
-			"nvim-tree/nvim-web-devicons", -- Used by the code bloxks
-		},
-		opts = {},
-	},
-
-	{
-		"zk-org/zk-nvim",
-		opts = {
-			picker = "telescope",
-			auto_attach = {
-				enabled = true,
-				filetypes = { "markdown" },
-			},
-		},
-		ft = { "markdown" },
-		config = function(_, opts)
-			require("zk").setup(opts)
+		"toppair/peek.nvim",
+		event = { "VeryLazy" },
+		build = "deno task --quiet build:fast",
+		config = function()
+			require("peek").setup()
+			vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+			vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
 		end,
 	},
 }

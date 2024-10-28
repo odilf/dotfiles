a4fd95e5dfd4384d2661a03bf00500c73a8e55df
Odilf <odysseas.maheras@gmail.com>
2024-10-28 13:09:13 +0100

    nix: remove all immich and docker stuff

--- nix/hosts/uoh-server/configuration.nix
+++ nix/hosts/uoh-server/configuration.nix
@@ -127,25 +127,6 @@
     };
 
     jellyfin.enable = true;
-
-    # immich = {
-    #   enable = true;
-    #   database.user = "postgres";
-    #   database.name = "immich";
-    #   database.createDB = false;
-    # };
-  };
-
-  virtualisation = {
-    podman = {
-      enable = true;
-
-      # Create a `docker` alias for podman, to use it as a drop-in replacement
-      dockerCompat = true;
-
-      # Required for containers under podman-compose to be able to talk to each other.
-      defaultNetwork.settings.dns_enabled = true;
-    };
   };
 
   # Open ports in the firewall.

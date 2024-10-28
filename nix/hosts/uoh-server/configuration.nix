a5464efbabc144de1d460affaa5ed7d40aa68344
Odilf <odysseas.maheras@gmail.com>
2024-10-28 13:08:54 +0100

    nix: remove specialization

--- nix/hosts/uoh-server/configuration.nix
+++ nix/hosts/uoh-server/configuration.nix
@@ -197,19 +197,6 @@
     };
   };
 
-  specialisation = {
-    desktop.configuration = {
-      services.xserver = {
-        enable = true;
-        displayManager.gdm.enable = true;
-        desktopManager.gnome.enable = true;
-      };
-
-      services.openssh.settings.X11Forwarding = true;
-      services.openssh.extraConfig = "X11UseLocalhost no";
-    };
-  };
-
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #

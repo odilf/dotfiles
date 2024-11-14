f6d495742f01666fe7ba671e712ea66a8a92b09c
Odilf <odysseas.maheras@gmail.com>
2024-11-14 13:16:18 +0100

    nix: fix nixbook for darwin

--- nix/modules/polyfill/nixos.nix
+++ nix/modules/polyfill/nixos.nix
@@ -8,5 +8,18 @@ in
 {
   options = {
     fonts.fontconfig = fill;
+    console = fill;
+    programs = {
+      hyprland = fill;
+      hyprlock = fill;
+      waybar = fill;
+    };
+
+    services = {
+      hypridle = fill;
+      xserver = fill;
+    };
+
+    systemd = fill;
   };
 }

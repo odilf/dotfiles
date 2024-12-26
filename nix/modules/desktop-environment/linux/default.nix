a64193073b5fc2277aa84e559456a2d4574b672f
Odilf <odysseas.maheras@gmail.com>
2024-12-26 09:30:04 +0100

    nix: remove default desktop-environment

--- nix/modules/desktop-environment/linux/default.nix
+++ nix/modules/desktop-environment/linux/default.nix
@@ -1,19 +1,9 @@
 {
-  pkgs,
-  lib,
-  config,
   ...
 }:
-let
-  cfg = config.desktop-environment;
-in
 {
   imports = [
     ./gnome.nix
     ./hyprland.nix
   ];
-
-  config = {
-    desktop-environment.hyprland.enable = lib.mkDefault true;
-  };
 }

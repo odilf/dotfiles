57dd28404efc06765bcb18287faf6174c474236f
odilf <odysseas.maheras@gmail.com>
2025-09-27 17:49:02 +0200

    add picard

--- nix/modules/bundles/productivity.nix
+++ nix/modules/bundles/productivity.nix
@@ -6,13 +6,23 @@
 }:
 let
   enable = user: config.custom.bundles."${user}".productivity.enable;
+  inherit (pkgs.stdenv.hostPlatform) isLinux;
 in
 {
   users.users."*" =
     user:
     lib.mkIf (enable user) {
-      packages = lib.optionals config.gui [
-        pkgs.zotero
-      ];
+      packages = lib.optionals config.gui (
+        [
+          pkgs.zotero
+        ]
+        ++ lib.optionals isLinux [
+          pkgs.picard
+        ]
+      );
     };
+
+  homebrew.casks = [
+    "musicbrainz-picard"
+  ];
 }

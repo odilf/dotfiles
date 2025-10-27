467f315a1450ba6f0592734a8dca6214d7b58f11
odilf <odysseas.maheras@gmail.com>
2025-10-27 08:10:26 +0100

    update

--- nix/modules/package-configurations/git.nix
+++ nix/modules/package-configurations/git.nix
@@ -2,17 +2,23 @@
   ...
 }:
 {
-  home-manager.users."*".programs.git = {
-    delta.enable = true;
-    userEmail = "odysseas.maheras@gmail.com";
-    userName = "odilf";
-    extraConfig = {
-      pull.rebase = true;
-      credential.helper = "cache";
-      rerere.enabled = true; # Resuse Recorded Resulution
-      fetch.prune = true;
-      gpg.format = "ssh";
-      user.signingkey = "~/.ssh/id_ed25519.pub";
+  home-manager.users."*".programs = {
+    git = {
+      settings = {
+        user.email = "odysseas.maheras@gmail.com";
+        user.name = "odilf";
+        pull.rebase = true;
+        credential.helper = "cache";
+        rerere.enabled = true; # Resuse Recorded Resulution
+        fetch.prune = true;
+        gpg.format = "ssh";
+        user.signingkey = "~/.ssh/id_ed25519.pub";
+      };
+    };
+
+    delta = {
+      enable = true;
+      enableGitIntegration = true;
     };
   };
 }

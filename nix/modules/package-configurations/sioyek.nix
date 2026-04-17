b94acc3c3999da7a0c56960f6493b2595cc84436
odilf <odysseas.maheras@gmail.com>
2026-04-17 13:25:30 +0200

    always open new sioyek windows

--- nix/modules/package-configurations/sioyek.nix
+++ nix/modules/package-configurations/sioyek.nix
@@ -4,7 +4,8 @@
   home-manager.users."*".programs.sioyek = {
     config = {
       ui_font = "IosevkaTerm Nerd Font";
-      # default_dark_mode = "1";
+      default_dark_mode = "1";
+      should_launch_new_window = "1";
     };
   };
 }
